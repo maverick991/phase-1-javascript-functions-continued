bdecb9aac05c90057e648ce05f6a1fdc4183bb8f
MaverickEdwards <r.edwards0917@gmail.com>
2022-11-04 21:23:23 -0400

    commit tree 8e90014af047bf0b3fabf4e31e45f35d253af042

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+    if (saturdayFun) {
+        return activity `dog`
+    }
+}
+let dog = 'bathe my dog'
+function mondayWork(workFromHome = 'go to the office') {
+    return `This Monday, I will ${workFromHome}.`
+}
+
+function wrapAdjective(emphatic = '*'){
+    const inner = function(always = "special"){
+        return `You are ${emphatic}${always}${emphatic}!`;
+    }
+return inner;
+}
